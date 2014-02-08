5bc781cc5f9b7451dc891756b4eee62bc082ab7b
Francisco J <nflamel@otrobloggeek.com>
2014-02-08 18:55:28 +0100

    Create config folder on first execution

--- lib/alfred/config/class_methods.rb
+++ lib/alfred/config/class_methods.rb
@@ -1,8 +1,10 @@
+require 'fileutils'
+
 module Alfred
   module Config
     module ClassMethods
       def base_path
-        "~/.alfred"
+        File.expand_path "~/.alfred"
       end
 
       def base_tasks_path
@@ -14,6 +16,21 @@ module Alfred
       end
 
       def init
+        init_base_path
+        init_config
+      end
+
+      protected
+
+      def init_base_path
+        unless File.directory? base_path
+          FileUtils.mkdir_p base_path
+          FileUtils.mkdir_p base_tasks_path
+          FileUtils.touch base_config_path
+        end
+      end
+
+      def init_config
         from_file base_config_path
       end
     end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -4,9 +4,6 @@ describe Alfred::Config do
   subject { Alfred::Config }
   before  { Alfred::Config.reset }
 
-  its(:base_path)       { should eq('~/.alfred') }
-  its(:base_tasks_path) { should eq('~/.alfred/tasks')}
-
   its("github.user")  { should be_nil }
   its("github.token") { should be_nil }
 
@@ -17,10 +14,19 @@ describe Alfred::Config do
 
     before do
       Alfred::Config.stub(:base_path).and_return(test_config_path)
-      Alfred::Config.init
+      Alfred::Config.send :init_config
     end
 
     its("github.user")  { should eq('dummy_user') }
     its("github.token") { should eq('dummy_token') }
+  end
+
+  context '.init_base_path' do
+    before { Alfred::Config.send :init_base_path }
+    subject { File }
+
+    it { should be_directory(Alfred::Config.base_path) }
+    it { should be_directory(Alfred::Config.base_tasks_path) }
+    it { should be_exists(Alfred::Config.base_config_path) }
   end
 end
