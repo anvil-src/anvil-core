30c5cfc313e3a2decc17a91284359923cfe7f954
Fran Casas <nflamel@otrobloggeek.com>
2014-02-15 17:33:42 +0100

    Avoid name collision

--- lib/tasks/help_task.rb
+++ lib/tasks/help_task.rb
@@ -4,17 +4,17 @@ class HelpTask < Alfred::Task
   description 'Help for the alfred tasks. Usage: alfred help TASK'
 
   parser do
-    arguments %w[task]
+    arguments %w[task_name]
   end
 
-  attr_reader :task
+  attr_reader :task_name
 
-  def initialize(task, options = {})
-    @task = task
+  def initialize(task_name, options = {})
+    @task_name = task_name
   end
 
   def task
-    klazz = Alfred::Task.from_name(task)
+    klazz = Alfred::Task.from_name(task_name)
     printf(klazz.help)
   end
 end
