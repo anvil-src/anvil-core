a36c275a028e74a9dc7c5f857628f529062064ab
Fran Casas <nflamel@otrobloggeek.com>
2014-02-15 18:30:11 +0100

    Command was only returning the command, not running it

--- lib/alfred/rubygems.rb
+++ lib/alfred/rubygems.rb
@@ -6,13 +6,13 @@ module Alfred
       def build(gemspec)
         line = Cocaine::CommandLine.new 'gem', 'build :gemspec'
 
-        line.command gemspec: gemspec
+        line.run gemspec: gemspec
       end
 
       def install(gem_file)
         line = Cocaine::CommandLine.new 'gem', 'install :gem_file'
 
-        line.command gem_file: gem_file
+        line.run gem_file: gem_file
       end
     end
   end
