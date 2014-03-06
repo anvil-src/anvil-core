a6e67d3f063a4d29003a691fae2e8d97deacc6ff
Fran Casas <nflamel@otrobloggeek.com>
2014-03-06 19:13:01 +0100

    Use new rspec expectations sintax

--- spec/lib/tasks/gem/bump_task_spec.rb
+++ spec/lib/tasks/gem/bump_task_spec.rb
@@ -4,12 +4,12 @@ require 'tasks/gem/bump_task'
 describe Gem::BumpTask do
   describe '#task' do
     subject { Gem::BumpTask.new :major }
+    before  { subject.stub(:read_version).and_return('2.0.0') }
 
     it 'bumps the version and writes it' do
-      subject.stub(:read_version).and_return('2.0.0')
-      subject.should_receive(:write_version)
+      expect(subject).to receive(:write_version)
 
-      subject.task.should eq('3.0.0')
+      expect(subject.task).to eq('3.0.0')
     end
   end
 end
