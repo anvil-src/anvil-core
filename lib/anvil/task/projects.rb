948b3b19ad9ecd0161b96da61636c8974045216e
Fran Casas <nflamel@otrobloggeek.com>
2014-06-04 10:52:48 +0200

    Allow to use on_each_project
    In addition to that, warn if chdir doesn't find the project's folder

--- lib/anvil/task/projects.rb
+++ lib/anvil/task/projects.rb
@@ -9,21 +9,30 @@ module Anvil
 
       def change_project(project)
         Dir.chdir(project_path(project))
+      rescue Errno::ENOENT
+        log_project_does_not_exists project
       end
 
       def on_project(project)
         Dir.chdir(project_path(project)) do
           yield(git)
         end
+      rescue Errno::ENOENT
+        log_project_does_not_exists project
       end
 
       def on_each_project(projects)
         projects.each do |project|
-          on_project(project) { |project_git| yield project_git }
+          on_project(project) { |project_git| yield project, project_git }
         end
       end
 
       protected
+
+      def log_project_does_not_exists(project)
+        logger.info "Anvil knows nothing about #{project}."
+        logger.info 'Please, check anvil help projects:add first to add the project to anvil.'
+      end
 
       def git
         Git.open(Dir.pwd)
