77ae053091b8388ee19c26127a1857109b7f5f91
Fran Casas <nflamel@otrobloggeek.com>
2014-06-25 10:52:08 +0200

    Use popen runner and clean env for bundler commands

--- lib/anvil/bundler.rb
+++ lib/anvil/bundler.rb
@@ -1,8 +1,11 @@
 module Anvil
   class Bundler
     def self.update_gem(gem_name)
-      line = Cocaine::CommandLine.new('bundle', 'update :gem')
-      line.run gem: gem_name
+      ::Bundler.with_clean_env do
+        Cocaine::CommandLine.runner = Cocaine::CommandLine::PopenRunner.new
+        line = Cocaine::CommandLine.new('bundle', "update #{gem_name}")
+        line.run
+      end
     end
   end
 end
